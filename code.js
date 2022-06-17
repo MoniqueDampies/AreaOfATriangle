2555aeb24bdf9ea1c316ecaa9954b701d217b5c7
MoniqueDampies <moniquedampies1861@gmail.com>
2022-06-17 12:20:08 +0200

    first

---
+++ code.js
@@ -0,0 +1,16 @@
+function Area() {
+  var side1 = parseInt(document.getElementById("side1").value);
+
+  var side2 = parseInt(document.getElementById("side2").value);
+
+  var side3 = parseInt(document.getElementById("side3").value);
+
+  console.log(typeof side1);
+  var s = (side1 + side2 + side3) / 2;
+
+  var area = Math.sqrt(s * ((s - side1) * (s - side2) * (s - side3)));
+
+  var area1 = area.toFixed(2);
+
+  document.getElementById("display").innerHTML = area1;
+}
